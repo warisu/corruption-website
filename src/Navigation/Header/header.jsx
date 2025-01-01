fb51bb74c047fd68bc773bf8979eab3947b9f65f
warisu <abdullahiabdulwaris@gmail.com>
2025-01-01 17:54:23 +0000

    fixing img

--- src/Navigation/Header/header.jsx
+++ src/Navigation/Header/header.jsx
@@ -5,9 +5,14 @@ import Navigation from '../Nav/nav.jsx';
 const Header = () => {
   return (
     <header className="header">
-      <div className='logo'>Corruption Watch</div>
-      <Navigation /> {/* Include the Navigation component */}
-    </header>
+      <div className='logo'>
+         <img id="header-img" src="http://www.talkingcorruptionsi.com/talking.jpg" loading="lazy"/>
+         <span>Talking Corruption & si </span>
+      </div>
+      <div>
+	  <Navigation /> {/* Include the Navigation component */}
+      </div>
+   </header>
   );
 };
 
