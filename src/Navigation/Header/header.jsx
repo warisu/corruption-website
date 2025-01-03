99b3c8595b0ff513f9b1772beaea2173677fdac6
warisu <abdullahiabdulwaris@gmail.com>
2025-01-03 04:10:15 +0000

    made harburger functional

--- src/Navigation/Header/header.jsx
+++ src/Navigation/Header/header.jsx
@@ -1,13 +1,35 @@
-import React from "react"; 
+import React, { useState } from "react"; 
 import './header.css';
 import Navigation from '../Nav/nav.jsx';
 
-const Header = () => {
+function Header () {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="header">
       <div className='logo'>
-         <img id="header-img" src="http://www.talkingcorruptionsi.com/talking.jpg" loading="lazy"/>
-         <span>Talking Corruption & si </span>
+        <div>
+	      <img className="header-img" src="https://www.talkingcorruptionsi.com/talking.jpg" alt="Talking Corruption & si" loading="lazy"/>
+           <span>Talking Corruption & si </span>
+        </div>
+        <div>
+	   <nav className="navbar">
+              <div className="menu-toggle" onClick={toggleMenu}>
+		 <span className="bar"></span>
+		 <span className="bar"></span>
+		 <span className="bar"></span>
+              </div>
+             <ul className={`nav-links ${isMenuOpen ? "active" : ""} `}>
+	       <li><a href="#feedback">Feedback</a></li>
+	       <li><a href="#news">News</a></li>
+	       <li><a href="#topics">Topics</a></li>
+             </ul>
+	    </nav>
+	</div>
       </div>
       <div>
 	  <Navigation /> {/* Include the Navigation component */}
